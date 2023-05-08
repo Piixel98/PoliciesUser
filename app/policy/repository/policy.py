06871ff467581365ee7834aaae353c1bcca152cc
Jordi Sánchez <jsanchezm@maas.es>
2023-05-08 13:27:28 +0200

    Dev repo policy

--- app/policy/repository/policy.py
+++ app/policy/repository/policy.py
@@ -1,10 +1,14 @@
 from abc import abstractmethod, ABCMeta
-
+from http import HTTPStatus
 from typing import Optional
 
+import httpx
 from uuid import UUID
 
+from app.policy.exception import PolicyNotFoundException
 from app.policy.schema.policy import PolicySchema
+from core.config import config
+from core.exceptions.base import InternalServerErrorException
 
 
 class PolicyRepository:
@@ -21,10 +25,40 @@ class PolicyRepository:
 
 class PolicyMockRepository(PolicyRepository):
     def __init__(self):
-        pass
+        self._timeout = httpx.Timeout(30.0, read=None)
+        self._client = httpx.Client(timeout=self._timeout)
+        self.headers = {
+            'Charset': 'UTF-8',
+            'Accept': 'application/json'
+        }
 
     async def get_by_id(self, policy_id: UUID) -> PolicySchema:
-        raise NotImplementedError
+        try:
+            response = self._client.get(f"{config.POLICY_MOCK_URL}", headers=self.headers)
+            if response.status_code == HTTPStatus.OK:
+                policies_found = [x for x in response.json()['policies'] if x['id'] == str(policy_id)]
+                if len(policies_found) == 0:
+                    raise PolicyNotFoundException
+                if len(policies_found) == 1:
+                    return PolicySchema.parse_obj(policies_found[0])
+            raise InternalServerErrorException
+        except PolicyNotFoundException:
+            raise PolicyNotFoundException
+        except Exception:
+            raise InternalServerErrorException
 
     async def get_by_user_id(self, user_id: UUID) -> list[PolicySchema]:
-        raise NotImplementedError
+        try:
+            response = self._client.get(f"{config.POLICY_MOCK_URL}", headers=self.headers)
+            if response.status_code == HTTPStatus.OK:
+                policies_found = [x for x in response.json()['policies'] if x['clientId'] == str(user_id)]
+                if len(policies_found) == 0:
+                    raise PolicyNotFoundException
+                if len(policies_found) > 0:
+                    return [PolicySchema.parse_obj(policy) for policy in policies_found]
+            raise InternalServerErrorException
+        except PolicyNotFoundException:
+            raise PolicyNotFoundException
+        except Exception:
+            raise InternalServerErrorException
+
