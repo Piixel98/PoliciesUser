4f9513f8ed67f333ec2646f093325b92840e1b10
Jordi Sánchez <jsanchezm@maas.es>
2023-05-08 13:20:07 +0200

    Implement logic in UserRepository

--- app/user/repository/user.py
+++ app/user/repository/user.py
@@ -1,6 +1,13 @@
 from abc import ABCMeta, abstractmethod
+from http import HTTPStatus
+
 from uuid import UUID
+import httpx
+
+from app.user.exception import UserNotFoundException
 from app.user.schema import UserSchema
+from core.config import config
+from core.exceptions.base import InternalServerErrorException
 
 
 class UserRepository:
@@ -21,13 +28,54 @@ class UserRepository:
 
 class UserMockRepository(UserRepository):
     def __init__(self):
-        pass
+        self._timeout = httpx.Timeout(30.0, read=None)
+        self._client = httpx.Client(timeout=self._timeout)
+        self.headers = {
+            'Charset': 'UTF-8',
+            'Accept': 'application/json'
+        }
 
     async def get_by_id(self, user_id: UUID) -> UserSchema:
-        raise NotImplementedError
+        try:
+            response = self._client.get(f"{config.CLIENT_MOCK_URL}", headers=self.headers)
+            if response.status_code == HTTPStatus.OK:
+                users_found = [x for x in response.json()['clients'] if x['id'] == str(user_id)]
+                if len(users_found) == 0:
+                    raise UserNotFoundException
+                if len(users_found) == 1:
+                    return UserSchema.parse_obj(users_found[0])
+            raise InternalServerErrorException
+        except UserNotFoundException:
+            raise UserNotFoundException
+        except Exception:
+            raise InternalServerErrorException
 
     async def get_by_name(self, name: str) -> UserSchema:
-        raise NotImplementedError
+        try:
+            response = self._client.get(f"{config.CLIENT_MOCK_URL}", headers=self.headers)
+            if response.status_code == HTTPStatus.OK:
+                users_found = [x for x in response.json()['clients'] if x['name'] == str(name)]
+                if len(users_found) == 0:
+                    raise UserNotFoundException
+                if len(users_found) == 1:
+                    return UserSchema.parse_obj(users_found[0])
+            raise InternalServerErrorException
+        except UserNotFoundException:
+            raise UserNotFoundException
+        except Exception:
+            raise InternalServerErrorException
 
     async def get_by_policy_id(self, policy_id: UUID) -> UserSchema:
-        raise NotImplementedError
+        try:
+            response = self._client.get(f"{config.POLICY_MOCK_URL}", headers=self.headers)
+            if response.status_code == HTTPStatus.OK:
+                policies_found = [x for x in response.json()['policies'] if x['id'] == str(policy_id)]
+                if len(policies_found) == 0:
+                    raise UserNotFoundException
+                if len(policies_found) == 1:
+                    return await self.get_by_id(user_id=policies_found[0]['clientId'])
+            raise InternalServerErrorException
+        except UserNotFoundException:
+            raise UserNotFoundException
+        except Exception:
+            raise InternalServerErrorException
